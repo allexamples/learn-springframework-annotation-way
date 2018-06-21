743268aff90c3fc1e4e224f12d18f1ff07417a27
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-06-21 22:06:40 +0530

    added basic spring web security

---
+++ src/main/java/org/allexample/springframework/annotationway/config/AppSecurityConfig.java
@@ -0,0 +1,21 @@
+package org.allexample.springframework.annotationway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication().withUser(userBuilder.username("James").password("test@123").roles("ADMIN"));
+        auth.inMemoryAuthentication().withUser(userBuilder.username("Rod").password("test@123").roles("EMPLOYEE"));
+        auth.inMemoryAuthentication().withUser(userBuilder.username("Martin").password("test@123").roles("LEADER"));
+    }
+}
---
+++ src/main/java/org/allexample/springframework/annotationway/initializer/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package org.allexample.springframework.annotationway.initializer;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
